142a95a7652477c1b671a9110ab017c85d6990f8
JuanWick <20243ds104@utez.edu.mx>
2025-09-04 13:54:29 -0600

    clase cuenta agregada

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,27 +1,27 @@
 public class Cuenta {
 
-        private String numeroCuenta;
-        private double saldo;
-        private Cliente titular;
+    private String numeroCuenta;
+    private double saldo;
+    private Cliente titular;
 
-        public Cuenta(String numeroCuenta, Cliente titular, double saldoInicial) {
-            this.numeroCuenta = numeroCuenta;
-            this.titular = titular;
-            this.saldo = saldoInicial;
-        }
+    public Cuenta(String numeroCuenta, Cliente titular, double saldoInicial) {
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
 
-        public void depositar(double cantidad) {
-            saldo += cantidad;
-        }
+    public void depositar(double cantidad) {
+        saldo += cantidad;
+    }
 
-        public boolean retirar(double cantidad) {
-            if (cantidad > saldo) return false;
-            saldo -= cantidad;
-            return true;
-        }
+    public boolean retirar(double cantidad) {
+        if (cantidad > saldo) return false;
+        saldo -= cantidad;
+        return true;
+    }
 
-        public double getSaldo() { return saldo; }
-        public String getNumeroCuenta() { return numeroCuenta; }
-        public Cliente getTitular() { return titular; }
+    public double getSaldo() { return saldo; }
+    public String getNumeroCuenta() { return numeroCuenta; }
+    public Cliente getTitular() { return titular; }
 
 }
