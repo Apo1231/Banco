a35c91dc7e2fd925a2d32c524b028fe76b75f679
Max432122 <20243ds175@utez.edu.mx>
2025-09-04 14:12:46 -0600

    agregue la clase app

--- src/App.java
+++ src/App.java
@@ -1,3 +1,16 @@
-public class App {
+import java.util.ArrayList;
 
+public class App {
+    private ArrayList<Cuenta> cuentas = new ArrayList<>();
+
+    public void agregarCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
+
+    public Cuenta buscarCuenta(String numero) {
+        for (Cuenta c : cuentas) {
+            if (c.getNumeroCuenta().equals(numero)) return c;
+        }
+        return null;
+    }
 }
