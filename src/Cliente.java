6c92a37bebb0f1833a664ec8cbc6789164649c37
Apo1231 <20243ds166@utez.edu.mx>
2025-09-04 13:38:34 -0600

    Cliente como clase, terminado

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,15 @@
 public class Cliente {
+    private String nombre;
+    private String direccion;
+    private String id;
+
+    public Cliente(String nombre, String direccion, String id) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.id = id;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getDireccion() { return direccion; }
+    public String getId() { return id; }
 }
