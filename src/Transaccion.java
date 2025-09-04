8c7b1b29014177e250ebfb3f6bf1eb264bda963f
Dulce <20243ds105@utez.edu.mx>
2025-09-04 14:01:09 -0600

    Clase de transaccion

--- src/Transaccion.java
+++ src/Transaccion.java
@@ -1,2 +1,15 @@
 public class Transaccion {
+    private String tipo; // "Depósito" o "Retiro"
+    private double monto;
+    private String fecha;
+
+    public Transaccion(String tipo, double monto, String fecha) {
+        this.tipo = tipo;
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+    public String getTipo() { return tipo; }
+    public double getMonto() { return monto; }
+    public String getFecha() { return fecha; }
 }
